435caf2a398e4e6e5d0fb86a396123760cee379c
btt46 <tuanthanhbui46@gmail.com>
2021-07-28 23:25:52 +0900

    add files

--- evaluate.sh
+++ evaluate.sh
@@ -8,25 +8,31 @@ BIN_DATA=$DATA_FOLDER/bin-data
 MODEL=$PWD/models/model/checkpoint_best.pt
 
 BLEU=$PWD/mosesdecoder/scripts/generic/multi-bleu.perl
-REF_EN=$DATA_FOLDER/data/test.en
-REF_VI=$DATA_FOLDER/data/test.vi
 
 mkdir -p $PWD/test
 TEST=$PWD/test
 
+REF_EN=$DATA_FOLDER/data/test.en
+REF_VI=$DATA_FOLDER/data/test.vi
+
+HYP_EN=$TEST/test.en
+HYP_VI=$TEST/test.vi
+
 CUDA_VISIBLE_DEVICES=GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
             --input $PROCESSED_DATA/test.src \
             --path $MODEL \
-            --beam 5 | tee $TEST/test.result
+            --beam 5 | tee $TEST/test.translation
 
-cat $TEST/test.result | head -n 1268 | sed -r 's/(@@ )|(@@ ?$)//g'  > test.result.vi
-cat $TEST/test.result | tail -n +1269 | sed -r 's/(@@ )|(@@ ?$)//g' > test.result.en
+grep ^H $TEST/test.translation| cut -f3- > test.result
+
+cat $TEST/test.result | head -n 1268 | sed -r 's/(@@ )|(@@ ?$)//g'  > $HYP_VI
+cat $TEST/test.result | tail -n +1269 | sed -r 's/(@@ )|(@@ ?$)//g' > $HYP_EN
 
 # English to Vietnamese
 echo "En > Vi"
-env LC_ALL=en_US.UTF-8 perl BLEU REF_VI < test.result.vi
+env LC_ALL=en_US.UTF-8 perl BLEU REF_VI < $HYP_VI
 
 # Vietnamese to English
 echo "Vi > En"
-env LC_ALL=en_US.UTF-8 perl BLEU REF_EN < test.result.en
+env LC_ALL=en_US.UTF-8 perl BLEU REF_EN < $HYP_EN
 
