0a480ace62b04b3bf5d7fa943ab5df710bb472af
btt46 <tuanthanhbui46@gmail.com>
2021-08-31 15:26:43 +0900

    refactor

--- evaluate-oneway.sh
+++ evaluate-oneway.sh
@@ -31,7 +31,7 @@ CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA 
             --path $MODEL \
             --beam 5 | tee ${TEST}/translation.result.${TGT}
 
-grep ^H $TEST/translation.result.${TGT}| cut -f3 > $TEST/test.result
+grep ^H $TEST/translation.result.${TGT}| cut -f3 > $TEST/test.result.${TGT}
 
 # detruecase
 $DETRUECASER < ${TEST}/test.result.${TGT} > ${TEST}/detruecase.${TGT}
