8e1c737124ef6977215a80c37aee3d1f50287669
btt46 <tuanthanhbui46@gmail.com>
2021-09-04 19:46:01 +0900

    refactor

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -56,58 +56,58 @@ DATA_NAME="valid test"
 # copy processed-data to new processed data
 for SET in $DATA_NAME ; do
 	echo "{SET}...."
-	cat $PROCESSED_DATA/${SET}.src > ${NEW_PROCESSED_DATA}/${SET}.src
-	cat $PROCESSED_DATA/${SET}.tgt > ${NNEW_PROCESSED_DATA}/${SET}.tgt
+	cat ${PROCESSED_DATA}/${SET}.src > ${NEW_PROCESSED_DATA}/${SET}.src
+	cat ${PROCESSED_DATA}/${SET}.tgt > ${NEW_PROCESSED_DATA}/${SET}.tgt
 done
 
 echo "old data"
-cat $PROCESSED_DATA/train.src | head -n 133317 |  > $NEW_PROCESSED_DATA/old.src.en
-cat $PROCESSED_DATA/train.src | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.vi
-cat $PROCESSED_DATA/train.tgt | head -n 133317 |  > $NEW_PROCESSED_DATA/old.tgt.vi
-cat $PROCESSED_DATA/train.tgt | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.en
+cat ${PROCESSED_DATA}/train.src | head -n 133317 |  > ${NEW_PROCESSED_DATA}/old.src.en
+cat ${PROCESSED_DATA}/train.src | tail -n +133318 |  > ${NEW_PROCESSED_DATA}/old.src.vi
+cat ${PROCESSED_DATA}/train.tgt | head -n 133317 |  > ${NEW_PROCESSED_DATA}/old.tgt.vi
+cat ${PROCESSED_DATA}/train.tgt | tail -n +133318 |  > ${NEW_PROCESSED_DATA}/old.src.en
 
 echo "new data"
 
-cat  $NEW_PROCESSED_DATA/old.src.en > $NEW_PROCESSED_DATA/train.src
-cat  $NEW_PROCESSED_DATA/old.tgt.vi > $NEW_PROCESSED_DATA/train.tgt
+cat  ${NEW_PROCESSED_DATA}/old.src.en > ${NEW_PROCESSED_DATA}/train.src
+cat  ${NEW_PROCESSED_DATA}old.tgt.vi > ${NEW_PROCESSED_DATA}/train.tgt
 
 
-cat ${NEW_DATA}/new.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
+cat ${NEW_DATA}/new.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' >>  ${NEW_PROCESSED_DATA}/train.src
 cat ${BPE_DATA}/train.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.vi
-cat $NEW_DATA/train.vi >> $NEW_PROCESSED_DATA/train.tgt
+cat ${NEW_DATA}/train.vi >> ${NEW_PROCESSED_DATA}/train.tgt
 
-cat  $NEW_PROCESSED_DATA/old.src.vi >> $NEW_PROCESSED_DATA/train.src
-cat  $NEW_PROCESSED_DATA/old.tgt.en >> $NEW_PROCESSED_DATA/train.tgt
+cat  ${NEW_PROCESSED_DATA}/old.src.vi >> ${NEW_PROCESSED_DATA}/train.src
+cat  ${NEW_PROCESSED_DATA}/old.tgt.en >> ${NEW_PROCESSED_DATA}/train.tgt
 
 cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
-cat  ${NEW_DATA}/new.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
+cat  ${NEW_DATA}/new.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' >>  ${NEW_PROCESSED_DATA}/train.src
 cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
-cat $NEW_DATA/train.en >> $NEW_PROCESSED_DATA/train.tgt
+cat ${NEW_DATA}/train.en >> ${NEW_PROCESSED_DATA}/train.tgt
 
 # learn bpe model with training data
 
 echo "=> LEARNING BPE MODEL: $BPE_MODEL"
 subword-nmt learn-joint-bpe-and-vocab --input ${NEW_PROCESSED_DATA}/train.src ${NEW_PROCESSED_DATA}/train.tgt \
-				-s $BPESIZE -o $NEW_BPE_MODEL/code.${BPESIZE}.bpe \
-				--write-vocabulary $NEW_BPE_MODEL/train.src.vocab $NEW_BPE_MODEL/train.tgt.vocab 
+				-s $BPESIZE -o ${NEW_BPE_MODEL}/code.${BPESIZE}.bpe \
+				--write-vocabulary ${NEW_BPE_MODEL}/train.src.vocab ${NEW_BPE_MODEL}/train.tgt.vocab 
 
 
 # apply sub-word segmentation
 
-
+DATA_NAME="train valid test"
 for SET in $DATA_NAME; do
-    subword-nmt apply-bpe -c $NEW_BPE_MODEL/code.${BPESIZE}.bpe < ${NEW_PROCESSED_DATA}/${SET}.src > $NEW_BPE_DATA/${SET}.src 
-    subword-nmt apply-bpe -c $NEW_BPE_MODEL/code.${BPESIZE}.bpe < ${NEW_PROCESSED_DATA}/${SET}.tgt > $NEW_BPE_DATA/${SET}.tgt
+    subword-nmt apply-bpe -c ${NEW_BPE_MODEL}/code.${BPESIZE}.bpe < ${NEW_PROCESSED_DATA}/${SET}.src > ${NEW_BPE_DATA}/${SET}.src 
+    subword-nmt apply-bpe -c ${NEW_BPE_MODEL}/code.${BPESIZE}.bpe < ${NEW_PROCESSED_DATA}/${SET}.tgt > ${NEW_BPE_DATA}/${SET}.tgt
 done
 
 
 # binarize train/valid/test
 
 fairseq-preprocess -s src -t tgt \
-			--destdir $NEW_BIN_DATA \
-			--trainpref $NEW_BPE_DATA/train \
-			--validpref $NEW_BPE_DATA/valid \
-			--testpref $NEW_BPE_DATA/test \
+			--destdir ${NEW_BIN_DATA} \
+			--trainpref ${NEW_BPE_DATA}/train \
+			--validpref ${NEW_BPE_DATA}/valid \
+			--testpref ${NEW_BPE_DATA}/test \
 			--joined-dictionary \
 			--workers 32 
 
