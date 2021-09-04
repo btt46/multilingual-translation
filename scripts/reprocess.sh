d0fce14eb2068c80b14d86abc5b1782bcdec17ae
btt46 <tuanthanhbui46@gmail.com>
2021-09-04 19:31:25 +0900

    refactor

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -22,7 +22,7 @@ BIN_DATA=$DATA_FOLDER/bin-data
 ONEWAYDATA=$DATA_FOLDER/oneway
 BPE_DATA=$ONEWAYDATA/bpe-data
 
-DATA_NAME="valid test"
+DATA_NAME="train valid test"
 TRUECASED_DATA=$DATA_FOLDER/truecased
 
 mkdir -p $NEW_DATA_FOLDER
@@ -34,30 +34,30 @@ mkdir -p $NEW_DATA
 mkdir -p $NEW_PROCESSED_DATA
 mkdir -p $NEW_BPE_MODEL
 
-# # prepare data for the bidirectional model
-# echo "=> PREPROCESSING en <> vi DATA: $PWD....."
+# prepare data for the bidirectional model
+echo "=> PREPROCESSING en <> vi DATA: $PWD....."
 
-# cat ${BPE_DATA}/train.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' > ${TRANSLATION_DATA}/translation.en
-# cat ${BPE_DATA}/train.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' > ${TRANSLATION_DATA}/translation.vi
-
-
-# CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
-#             --input ${TRANSLATION_DATA}/translation.en \
-#             --path $MODEL \
-#             --beam 5 | tee $NEW_DATA/result.vi
+cat ${BPE_DATA}/train.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' > ${TRANSLATION_DATA}/translation.en
+cat ${BPE_DATA}/train.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' > ${TRANSLATION_DATA}/translation.vi
 
 
-# CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
-#             --input ${TRANSLATION_DATA}/translation.vi \
-#             --path $MODEL \
-#             --beam 5 | tee $NEW_DATA/result.en
+CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
+            --input ${TRANSLATION_DATA}/translation.en \
+            --path $MODEL \
+            --beam 5 | tee $NEW_DATA/result.vi
+
+
+CUDA_VISIBLE_DEVICES=$GPUS env LC_ALL=en_US.UTF-8 fairseq-interactive $BIN_DATA \
+            --input ${TRANSLATION_DATA}/translation.vi \
+            --path $MODEL \
+            --beam 5 | tee $NEW_DATA/result.en
 
 # grep ^H ${NEW_DATA}/result.vi | cut -f3 > ${NEW_DATA}/data.vi
-# grep ^H ${NEW_DATA}/result.en | cut -f3 > ${NEW_DATA}/data.en
+grep ^H ${NEW_DATA}/result.en | cut -f3 > ${NEW_DATA}/data.en
 
-# # 普通文字に戻す
-# cat ${NEW_DATA}/data.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > $NEW_DATA/new.vi
-# cat ${NEW_DATA}/data.en  | sed -r 's/(@@ )|(@@ ?$)//g' > $NEW_DATA/new.en
+# 普通文字に戻す
+cat ${NEW_DATA}/data.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > $NEW_DATA/new.vi
+cat ${NEW_DATA}/data.en  | sed -r 's/(@@ )|(@@ ?$)//g' > $NEW_DATA/new.en
 
 # copy processed-data to new processed data
 for SET in $DATA_NAME ; do
@@ -65,28 +65,43 @@ for SET in $DATA_NAME ; do
 	cat $PROCESSED_DATA/${SET}.tgt > $NEW_PROCESSED_DATA/${SET}.tgt
 done
 
-cat $PROCESSED_DATA/train.src | head -n 133317 |  > $NEW_PROCESSED_DATA/old.src.en
-cat $PROCESSED_DATA/train.src | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.vi
-cat $PROCESSED_DATA/train.tgt | head -n 133317 |  > $NEW_PROCESSED_DATA/old.tgt.vi
-cat $PROCESSED_DATA/train.tgt | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.en
-
-
-cat  $NEW_PROCESSED_DATA/old.src.en > $NEW_PROCESSED_DATA/train.src
-cat  $NEW_PROCESSED_DATA/old.tgt.vi > $NEW_PROCESSED_DATA/train.tgt
-
-
+cat ${TRANSLATION_DATA}/translation.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.vi
 cat  ${NEW_DATA}/new.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
-cat ${BPE_DATA}/train.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.vi
 cat $NEW_DATA/train.vi >> $NEW_PROCESSED_DATA/train.tgt
 
-cat  $NEW_PROCESSED_DATA/old.src.vi >> $NEW_PROCESSED_DATA/train.src
-cat  $NEW_PROCESSED_DATA/old.tgt.en >> $NEW_PROCESSED_DATA/train.tgt
-
-cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
+cat ${TRANSLATION_DATA}/translation.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
 cat  ${NEW_DATA}/new.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
-cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
-cat $NEW_DATA/train.en >> $NEW_PROCESSED_DATA/train.tgt
+cat $NEW_DATA/new.en >> $NEW_PROCESSED_DATA/train.tgt
 
+
+
+# copy processed-data to new processed data
+# for SET in $DATA_NAME ; do
+# 	cat $PROCESSED_DATA/${SET}.src > $NEW_PROCESSED_DATA/${SET}.src
+# 	cat $PROCESSED_DATA/${SET}.tgt > $NEW_PROCESSED_DATA/${SET}.tgt
+# done
+
+# cat $PROCESSED_DATA/train.src | head -n 133317 |  > $NEW_PROCESSED_DATA/old.src.en
+# cat $PROCESSED_DATA/train.src | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.vi
+# cat $PROCESSED_DATA/train.tgt | head -n 133317 |  > $NEW_PROCESSED_DATA/old.tgt.vi
+# cat $PROCESSED_DATA/train.tgt | tail -n +133318 |  > $NEW_PROCESSED_DATA/old.src.en
+
+
+# cat  $NEW_PROCESSED_DATA/old.src.en > $NEW_PROCESSED_DATA/train.src
+# cat  $NEW_PROCESSED_DATA/old.tgt.vi > $NEW_PROCESSED_DATA/train.tgt
+
+
+# cat ${NEW_DATA}/new.en | awk -vtgt_tag="<e2v>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
+# cat ${BPE_DATA}/train.vi | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.vi
+# cat $NEW_DATA/train.vi >> $NEW_PROCESSED_DATA/train.tgt
+
+# cat  $NEW_PROCESSED_DATA/old.src.vi >> $NEW_PROCESSED_DATA/train.src
+# cat  $NEW_PROCESSED_DATA/old.tgt.en >> $NEW_PROCESSED_DATA/train.tgt
+
+# cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
+# cat  ${NEW_DATA}/new.vi | awk -vtgt_tag="<v2e>" '{ print tgt_tag" "$0 }' >>  $NEW_PROCESSED_DATA/train.src
+# cat ${BPE_DATA}/train.en | sed -r 's/(@@ )|(@@ ?$)//g'  > ${NEW_DATA}/train.en
+# cat $NEW_DATA/train.en >> $NEW_PROCESSED_DATA/train.tgt
 
 # learn bpe model with training data
 
