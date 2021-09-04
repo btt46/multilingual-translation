15305cbb6eceac0d6927f73be65acbfa3c5138f5
btt46 <tuanthanhbui46@gmail.com>
2021-09-04 19:46:32 +0900

    refactor

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -55,7 +55,7 @@ mkdir -p $NEW_BPE_MODEL
 DATA_NAME="valid test"
 # copy processed-data to new processed data
 for SET in $DATA_NAME ; do
-	echo "{SET}...."
+	echo "${SET}...."
 	cat ${PROCESSED_DATA}/${SET}.src > ${NEW_PROCESSED_DATA}/${SET}.src
 	cat ${PROCESSED_DATA}/${SET}.tgt > ${NEW_PROCESSED_DATA}/${SET}.tgt
 done
