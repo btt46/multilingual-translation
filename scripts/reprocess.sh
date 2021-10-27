db4d89dc6c2a4e69d4d2992af2e2f01cdec872c3
btt46 <tuanthanhbui46@gmail.com>
2021-10-28 08:44:41 +0900

    refactor evaluate

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -10,7 +10,7 @@ NEW_BPE_DATA=$NEW_DATA_FOLDER/bpe-data-${NUM}
 NEW_BIN_DATA=$NEW_DATA_FOLDER/bin-data-${NUM}
 TRANSLATION_DATA=$NEW_DATA_FOLDER/translation-data
 NEW_DATA=$NEW_DATA_FOLDER/new-data
-NEW_PROCESSED_DATA=$NEW_DATA_FOLDER/processed-data
+NEW_PROCESSED_DATA=$NEW_DATA_FOLDER/processed-data-${NUM}
 PROCESSED_DATA=$DATA_FOLDER/processed-data
 
 # The model used for evaluate
