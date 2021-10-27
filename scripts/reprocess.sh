e405d0d82184198f01cc676c2528f9b3db7fbad6
btt46 <tuanthanhbui46@gmail.com>
2021-10-28 08:49:35 +0900

    refactor evaluate

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -44,8 +44,8 @@ DATA_NAME="valid test"
 
 # copy processed-data to new processed data
 for SET in $DATA_NAME ; do
-	cat $PROCESSED_DATA/${SET}.src > $NEW_PROCESSED_DATA/${SET}.src.${NUM}
-	cat $PROCESSED_DATA/${SET}.tgt > $NEW_PROCESSED_DATA/${SET}.tgt.${NUM}
+	cat $PROCESSED_DATA/${SET}.src > $NEW_PROCESSED_DATA/${SET}.src
+	cat $PROCESSED_DATA/${SET}.tgt > $NEW_PROCESSED_DATA/${SET}.tgt
 done
 
 cat ${TRANSLATION_DATA}/translation.vi | sed -r 's/(@@ )|(@@ ?$)|(<v2e> )//g'  > ${NEW_DATA}/train.vi.${NUM}
