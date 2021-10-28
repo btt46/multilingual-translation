3733abb447ff67c21ee3281ecf5015c5e3dd0c4a
btt46 <tuanthanhbui46@gmail.com>
2021-10-28 20:35:29 +0900

    refactor

--- evaluate.sh
+++ evaluate.sh
@@ -17,7 +17,7 @@ if [ $NUM -gt 1 ]; then
 	BPE_DATA=$DATA_FOLDER/new-data-random/bpe-data-${NUM}
 fi
 
-if [ $NUM==1 ]; then
+if [ $NUM -eq 1 ]; then
 	echo "$NUM default"
 	BIN_DATA=$DATA_FOLDER/new-data-random/bin-data
 	BPE_DATA=$DATA_FOLDER/new-data-random/bpe-data
