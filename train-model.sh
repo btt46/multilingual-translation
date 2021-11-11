679fdd3712145aea9a50d97763ca90bd3e199ad3
btt46 <tuanthanhbui46@gmail.com>
2021-11-11 22:15:11 +0900

    refactor

--- train-model.sh
+++ train-model.sh
@@ -4,6 +4,7 @@ GPUS=$1
 MODEL_NAME=$2
 NUM=$3
 EPOCHS=$4
+FLAG=$5
 echo "GPU=${GPUS}"
 DATA=$PWD/data/new-data-random/bin-data-${NUM}	
 MODEL=$PWD/models/${MODEL_NAME}
@@ -12,32 +13,60 @@ mkdir -p $PWD/log
 LOG=$PWD/log
 PRETRAINED_MODEL=$PWD/models/model.bi/checkpoint_best.pt
 
+if [ $FLAG -gt 0 ]; then
+	CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
+	            --log-interval 100 \
+				--log-format json \
+				--max-epoch ${EPOCHS} \
+	    		--optimizer adam --lr 0.0001 \
+				--clip-norm 0.0 \
+				--max-tokens 4000 \
+				--no-progress-bar \
+				--log-interval 100 \
+				--min-lr '1e-09' \
+				--weight-decay 0.0001 \
+				--criterion label_smoothed_cross_entropy \
+				--label-smoothing 0.1 \
+				--lr-scheduler inverse_sqrt \
+				--warmup-updates 4000 \
+				--warmup-init-lr '1e-08' \
+				--adam-betas '(0.9, 0.98)' \
+				--arch transformer_iwslt_de_en \
+				--dropout 0.1 \
+				--attention-dropout 0.1 \
+				--share-decoder-input-output-embed \
+				--share-all-embeddings \
+				--finetune-from-model $PRETRAINED_MODEL\
+				--save-dir $MODEL \
+				2>&1 | tee $LOG/log.train.${MODEL_NAME}
+fi
 
-CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
-            --log-interval 100 \
-			--log-format json \
-			--max-epoch ${EPOCHS} \
-    		--optimizer adam --lr 0.0001 \
-			--clip-norm 0.0 \
-			--max-tokens 4000 \
-			--no-progress-bar \
-			--log-interval 100 \
-			--min-lr '1e-09' \
-			--weight-decay 0.0001 \
-			--criterion label_smoothed_cross_entropy \
-			--label-smoothing 0.1 \
-			--lr-scheduler inverse_sqrt \
-			--warmup-updates 4000 \
-			--warmup-init-lr '1e-08' \
-			--adam-betas '(0.9, 0.98)' \
-			--arch transformer_iwslt_de_en \
-			--dropout 0.1 \
-			--attention-dropout 0.1 \
-			--share-decoder-input-output-embed \
-			--share-all-embeddings \
-			--finetune-from-model $PRETRAINED_MODEL\
-			--save-dir $MODEL \
-			2>&1 | tee $LOG/log.train.${MODEL_NAME}
+if [ $FLAG -eq 0 ]; then
+	CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
+	            --log-interval 100 \
+				--log-format json \
+				--max-epoch ${EPOCHS} \
+	    		--optimizer adam --lr 0.0001 \
+				--clip-norm 0.0 \
+				--max-tokens 4000 \
+				--no-progress-bar \
+				--log-interval 100 \
+				--min-lr '1e-09' \
+				--weight-decay 0.0001 \
+				--criterion label_smoothed_cross_entropy \
+				--label-smoothing 0.1 \
+				--lr-scheduler inverse_sqrt \
+				--warmup-updates 4000 \
+				--warmup-init-lr '1e-08' \
+				--adam-betas '(0.9, 0.98)' \
+				--arch transformer_iwslt_de_en \
+				--dropout 0.1 \
+				--attention-dropout 0.1 \
+				--share-decoder-input-output-embed \
+				--share-all-embeddings \
+				--save-dir $MODEL \
+				2>&1 | tee $LOG/log.train.${MODEL_NAME}
+fi
 
 echo "TRAINING LOG: $LOG"
 # --finetune-from-model $PRETRAINED_MODEL\
