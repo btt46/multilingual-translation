66d308076f7cf1592248ecda86669931c750151c
btt46 <tuanthanhbui46@gmail.com>
2021-11-13 18:23:00 +0900

    fix bug

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -27,8 +27,8 @@ MERGE_FILE=$PWD/scripts/merge-file.py
 
 TRUECASED_DATA=$DATA_FOLDER/truecased
 
-rm -rf $NEW_PROCESSED_DATA
-rm -rf $NEW_BIN_DATA
+rm  $NEW_PROCESSED_DATA/*
+rm  $NEW_BIN_DATA/*
 
 mkdir -p $NEW_DATA_FOLDER
 mkdir -p $NEW_DATA
