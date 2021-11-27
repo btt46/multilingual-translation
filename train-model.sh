044b26ca17c9eed13d863af739895b383d8d866a
btt46 <tuanthanhbui46@gmail.com>
2021-11-27 19:15:35 +0900

    upload for ibt

--- train-model.sh
+++ train-model.sh
@@ -11,10 +11,73 @@ mkdir -p $MODEL
 mkdir -p $PWD/log
 LOG=$PWD/log
 
-if [ $FLAG -ge 0 ]; then
+if [ $NUM -lt 9 ]; then
+	if [ $FLAG -ge 0 ] ; then
+		DATA=$PWD/data/new-data-random/bin-data-${NUM}	
+		echo "bi+BT ${MODEL}"
+		PRETRAINED_MODEL=$PWD/models/model.bi/checkpoint_best.pt
+
+		CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
+		            --log-interval 100 \
+					--log-format json \
+					--max-epoch ${EPOCHS} \
+		    		--optimizer adam --lr 0.0001 \
+					--clip-norm 0.0 \
+					--max-tokens 4000 \
+					--no-progress-bar \
+					--log-interval 100 \
+					--min-lr '1e-09' \
+					--weight-decay 0.0001 \
+					--criterion label_smoothed_cross_entropy \
+					--label-smoothing 0.1 \
+					--lr-scheduler inverse_sqrt \
+					--warmup-updates 4000 \
+					--warmup-init-lr '1e-08' \
+					--adam-betas '(0.9, 0.98)' \
+					--arch transformer_iwslt_de_en \
+					--dropout 0.1 \
+					--attention-dropout 0.1 \
+					--share-decoder-input-output-embed \
+					--share-all-embeddings \
+					--finetune-from-model $PRETRAINED_MODEL\
+					--save-dir $MODEL \
+					2>&1 | tee $LOG/log.train.${MODEL_NAME}
+	fi
+
+	if [ $FLAG -lt 0 ]; then
+		echo "bi ${MODEL}"
+		DATA=$PWD/data/bin-data
+		CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
+		            --log-interval 100 \
+					--log-format json \
+					--max-epoch ${EPOCHS} \
+		    		--optimizer adam --lr 0.0001 \
+					--clip-norm 0.0 \
+					--max-tokens 4000 \
+					--no-progress-bar \
+					--log-interval 100 \
+					--min-lr '1e-09' \
+					--weight-decay 0.0001 \
+					--criterion label_smoothed_cross_entropy \
+					--label-smoothing 0.1 \
+					--lr-scheduler inverse_sqrt \
+					--warmup-updates 4000 \
+					--warmup-init-lr '1e-08' \
+					--adam-betas '(0.9, 0.98)' \
+					--arch transformer_iwslt_de_en \
+					--dropout 0.1 \
+					--attention-dropout 0.1 \
+					--share-decoder-input-output-embed \
+					--share-all-embeddings \
+					--save-dir $MODEL \
+					2>&1 | tee $LOG/log.train.${MODEL_NAME}
+	fi
+fi
+
+if [ $NUM -ge 9 ]; then
 	DATA=$PWD/data/new-data-random/bin-data-${NUM}	
-	echo "bi+BT ${MODEL}"
-	PRETRAINED_MODEL=$PWD/models/model.bi/checkpoint_best.pt
+	echo "bi+IBT ${MODEL}"
+	PRETRAINED_MODEL=$PWD/models/model.bi.BT0/checkpoint26.pt
 
 	CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
 	            --log-interval 100 \
@@ -39,35 +102,6 @@ if [ $FLAG -ge 0 ]; then
 				--share-decoder-input-output-embed \
 				--share-all-embeddings \
 				--finetune-from-model $PRETRAINED_MODEL\
-				--save-dir $MODEL \
-				2>&1 | tee $LOG/log.train.${MODEL_NAME}
-fi
-
-if [ $FLAG -lt 0 ]; then
-	echo "bi ${MODEL}"
-	DATA=$PWD/data/bin-data
-	CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $DATA -s src -t tgt \
-	            --log-interval 100 \
-				--log-format json \
-				--max-epoch ${EPOCHS} \
-	    		--optimizer adam --lr 0.0001 \
-				--clip-norm 0.0 \
-				--max-tokens 4000 \
-				--no-progress-bar \
-				--log-interval 100 \
-				--min-lr '1e-09' \
-				--weight-decay 0.0001 \
-				--criterion label_smoothed_cross_entropy \
-				--label-smoothing 0.1 \
-				--lr-scheduler inverse_sqrt \
-				--warmup-updates 4000 \
-				--warmup-init-lr '1e-08' \
-				--adam-betas '(0.9, 0.98)' \
-				--arch transformer_iwslt_de_en \
-				--dropout 0.1 \
-				--attention-dropout 0.1 \
-				--share-decoder-input-output-embed \
-				--share-all-embeddings \
 				--save-dir $MODEL \
 				2>&1 | tee $LOG/log.train.${MODEL_NAME}
 fi
